3b2a9b2e2f7781dd0f5c7ec0df9e4d69e2f37f5c
satelite54 <taehune94@gmail.com>
2021-01-18 13:56:39 +0900

    [Taeheon]  - dao, dto(Board, Order, Products, Users) 추가

---
+++ VapeWeb/src/DAO/dao.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class dao {
+	//DB 연결 객체 선언 
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public dao() {
+		try {
+			String url = "jdbc:mysql://localhost:3306/BBS?useSSL=false";
+			String user = "root";
+			String password = "1234";
+
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void closeAll() {	
+		try {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+}
