c19f79553e0bfc63bebcbdaf001b2d982d6d1ed0
parka01 <68191916+parka01@users.noreply.github.com>
2021-01-20 14:59:59 +0900

    [Ahyoung]src폴더 붙여넣기

---
+++ VapeWeb/src/DTO/Board.java
@@ -0,0 +1,56 @@
+package DTO;
+
+import java.sql.Date;
+
+public class Board {
+	private int BNum;
+	private String BTitle;
+	private String BContent;
+	private Date BDate;
+	private String id;
+	private int authority;
+	private int views;
+	
+	public int getBNum() {
+		return BNum;
+	}
+	public void setBNum(int bNum) {
+		BNum = bNum;
+	}
+	public String getBTitle() {
+		return BTitle;
+	}
+	public void setBTitle(String bTitle) {
+		BTitle = bTitle;
+	}
+	public String getBContent() {
+		return BContent;
+	}
+	public void setBContent(String bContent) {
+		BContent = bContent;
+	}
+	public Date getBDate() {
+		return BDate;
+	}
+	public void setBDate(Date bDate) {
+		BDate = bDate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getAuthority() {
+		return authority;
+	}
+	public void setAuthority(int authority) {
+		this.authority = authority;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+}
---
+++ VapeWeb/src/DTO/Products.java
@@ -0,0 +1,71 @@
+package DTO;
+
+import java.sql.Date;
+
+public class Products {
+	private String pname;
+	private String code;
+	private String manufacturer;
+	private int price;
+	private int stock;
+	private String imgname;
+	private Date adddate;
+	private String producttype;
+	private String Detailedimagepath;
+	
+	
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getImgname() {
+		return imgname;
+	}
+	public void setImgname(String imgname) {
+		this.imgname = imgname;
+	}
+	public Date getAdddate() {
+		return adddate;
+	}
+	public void setAdddate(Date adddate) {
+		this.adddate = adddate;
+	}
+	public String getProducttype() {
+		return producttype;
+	}
+	public void setProducttype(String producttype) {
+		this.producttype = producttype;
+	}
+	public String getDetailedimagepath() {
+		return Detailedimagepath;
+	}
+	public void setDetailedimagepath(String detailedimagepath) {
+		Detailedimagepath = detailedimagepath;
+	}
+}
