08a3d5102c7708140e0df66e875fa0489e226bd9
curong <73506250+curong@users.noreply.github.com>
2021-02-24 17:24:05 +0900

    git!

--- VapeWeb/WebContent/news.jsp
+++ VapeWeb/WebContent/news.jsp
@@ -1,26 +1,29 @@
-<%@page import="org.apache.naming.HandlerRef"%>
 <%@page import="crawl.naver"%>
+<%@page import="org.apache.naming.HandlerRef"%>
 <%@ page language="java" contentType="text/html; charset=utf-8"
 	pageEncoding="utf-8"%>
 <%@ include file="menu.jsp"%>
 <%@ include file="submenu.jsp"%>
 
 <%
-	naver Naver = new naver();
-	Naver.newsList();
-	ArrayList hrefList = new ArrayList();
-	ArrayList titleList = new ArrayList();
-	for(int i = 0 ; i < newsList.size(); i++){
-		titleList.add(i, newsList.get(i));
-		hrefList.add(i, newsList.get(i));
-	}
+
 %>
 
 <body>
 	<div>
-		<p>
-			<a href="<%=newsList.get(i) %>"><%= newsList %></a>
-		</p>
-		}
+		<table style="width:500px; height:500px; text-align: center; position: static; display: flex; border: solid 2px;">
+				<tr >
+						<td style="border: solid 2px"><a href="#">구</a></td>
+				</tr>			
+				<tr>
+						<td><a href="#">구</a></td>
+				</tr>			
+				<tr>
+						<td><a href="#">구</a></td>
+				</tr>			
+		</table>		
 	</div>
 </body>
+
+
+
---
+++ VapeWeb/src/crawl/naver.java
@@ -0,0 +1,30 @@
+package crawl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class naver {
+
+	public static void main(String[] args) throws IOException {
+				String search = "전자담배";
+					long startTime = System.currentTimeMillis();
+					for (int j = 0; j < 50; j+=10) {
+						String URL = "https://search.naver.com/search.naver?&where=news&query="+search+"&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:dd,p:all,a:all&mynews=0&start="+j+"&refresh_start=0";
+						Document doc = Jsoup.connect(URL).get();
+						List<String> title = doc.getElementsByClass("news_tit").eachText();
+						List<String> href = doc.getElementsByClass("news_tit").eachAttr("href");
+					for (int i = 0; i < title.size(); i++) {
+						System.out.println(title.get(i)+"\n"+href.get(i)+"\n");
+					}
+					
+			}
+					long endTime = System.currentTimeMillis();
+					System.out.println(endTime-startTime);
+		}}
+
+	
+
+
